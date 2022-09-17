a616080485ef68901242d61d2a968a5fc359b7ea
reisuta <in345vain@gmail.com>
2022-09-18 08:07:50 +0900

    4章:Rubyの文法を詳しく

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  # ページごとの完全なタイトルを返します。
+  def full_title(page_title = '')
+    base_title = 'Ruby on Rails Tutorial Sample App'
+    if page_title.empty?
+      base_title
+    else
+      page_title + ' | ' + base_title
+    end
+  end
+
+  # 大文字にして返す遊びメソッド
+  def yeller(args = []) # yeller(['o', 'l', 'd']) => "OLD"
+    return if args === []
+    args.map(&:upcase).join
+  end
 end
